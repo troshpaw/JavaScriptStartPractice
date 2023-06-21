a1a637022eb6e1e9948210c9690bd612daa88584
Pavel <troshpaw@gmail.com>
2023-06-21 10:51:33 +0300

    done 0.5 42

--- 42-is-array-sorted-or-not/my-solution.js
+++ 42-is-array-sorted-or-not/my-solution.js
@@ -16,6 +16,22 @@ const b = [4, 10, 14, 25, 25, 50]
 const c = [150, 132, 80, 40]
 const d = [15, 26, 10, 23, 85]
 
+const arraySortInfo = (array) => {
+    if (array.some(element => typeof element !== 'number')) {
+        return 'Некоторые элементы не являются числами'
+    }
+
+    if (array.every((element, index) => {})) {
+        return 'Массив отсортирован по возрастанию'
+    }
+
+    if (array.every((element, index) => {})) {
+        return 'Массив отсортирован по убыванию'
+    }
+
+    return 'Массив не отсортирован'
+}
+
 console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
 console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
 console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
