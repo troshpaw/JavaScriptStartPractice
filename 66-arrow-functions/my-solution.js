b3f4fd79527a1887f2dac0e60ada039a2d493a8c
Pavel <troshpaw@gmail.com>
2023-08-03 11:42:14 +0300

    done ...70

---
+++ 66-arrow-functions/my-solution.js
@@ -0,0 +1,22 @@
+/** ЗАДАЧА 66 - Стрелочные функции
+ *
+ * Замените обычные функции на стрелочные
+ */
+
+/*
+function mult(a, b) {
+	return a * b
+}
+*/
+
+const mult = (a,b) => a * b
+
+/*
+setTimeout(function () {
+	console.log(mult(5, 10))
+}, 1000)
+*/
+
+setTimeout(() => console.log(mult(5, 10)), 1000)
+// 50
+
