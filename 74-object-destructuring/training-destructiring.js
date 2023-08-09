a44082547e94890e0e9031708dc98d2239f6485b
Pavel <troshpaw@gmail.com>
2023-08-09 11:39:55 +0300

    75 in process

--- 74-object-destructuring/training-destructiring.js
+++ 74-object-destructuring/training-destructiring.js
@@ -1,4 +1,4 @@
-
+/*
 const person = {
     name: 'Nick',
     age: 20,
@@ -9,3 +9,22 @@ const infoForPerson = ({name: n, age: a, location: {country: country, city}}) =>
     ({n, a, country, city})
 
 console.log(infoForPerson(person)) // {n: Nick, a: 20, country: Spain, city: Madrid}
+*/
+
+// Ex. 2:
+const customer = {
+    name: 'Sherlock',
+    email: 's.h.@gmail.com',
+    age: 34,
+    address: {
+        streetNo: '221b Baker Street',
+        city: 'London',
+        country: 'UK'
+    }
+}
+
+const {name: nameCustomer, age: ageCustomer, address: {streetNo: streetNoCustomer, city: cityCustomer}} = customer
+
+console.log(nameCustomer) // Sherlock
+console.log(streetNoCustomer) // 221b Baker Street
+console.log(cityCustomer) // UK
---
+++ 75-iterate-over-string/my-solution.js
@@ -0,0 +1,15 @@
+/** ЗАДАЧА 75 - Перебор символов строки
+ *
+ * Подсчитайте количество строчных гласных букв в строке.
+ * Гласные буквы - a, e, i, o, u
+ */
+
+let vowelsCount = 0
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+const str = 'Today is the best day of my life'
+
+console.log(str[0])
+
+console.log(vowelsCount)
+// 9
