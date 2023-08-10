9bf18d292c73f6c75141e6499dd32f2b95523310
Pavel <troshpaw@gmail.com>
2023-08-10 10:27:21 +0300

    79 in process

--- 75-iterate-over-string/my-solution.js
+++ 75-iterate-over-string/my-solution.js
@@ -9,7 +9,27 @@ const vowels = ['a', 'e', 'i', 'o', 'u']
 
 const str = 'Today is the best day of my life'
 
-console.log(str[0])
+/* method 1:
+const countVowels = (str, vowels) => {
+    str.split('').forEach((symbol) => {
+        if (vowels.includes(symbol)) {
+            vowelsCount ++
+        }
+    })    
+}
+*/
+
+// method 2:
+const countVowels = (str, vowels) => {
+    for (const symbol of str) {
+        if (vowels.includes(symbol)) {
+            vowelsCount += 1
+        }
+    }
+}
+
+countVowels(str, vowels)
 
 console.log(vowelsCount)
 // 9
+
---
+++ 79-iterate-over-object/my-solution.js
@@ -0,0 +1,36 @@
+/** ЗАДАЧА 79 - Перебор свойств объекта
+ *
+ * 1. Создайте функцию "sumObjectValues", которая будет суммировть
+ * все значения свойств, которые являются числами.
+ *
+ * 2. Сумму чисел необходимо вернуть из функции
+ *
+ * 3. Убедитесь, что итерация выполняется только
+ * по собственным свойствам объекта
+ */
+
+const objectWithNumbers = {
+	a: 10,
+	b: 20,
+	c: 'string',
+	d: 12,
+}
+
+// method 1:
+const sumObjectValues = (object) => {
+    let sum = 0
+    for (let property in object) {
+        if (typeof object[property] === 'number') {
+            console.log(property, '=', object[property])
+            sum += object[property]
+        }
+    }
+    return sum
+}
+
+// method 2:
+// ...
+
+const result = sumObjectValues(objectWithNumbers)
+console.log('sum =', result)
+//42
